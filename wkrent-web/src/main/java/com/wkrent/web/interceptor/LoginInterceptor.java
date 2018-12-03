5dd06fead8a1bb8942f4337dd4893c7d8d35003e
chengan <597049629@qq.com>
2018-12-03 23:55:18 +0800

    2018-12-02 添加token认证

--- wkrent-web/src/main/java/com/wkrent/web/interceptor/LoginInterceptor.java
+++ wkrent-web/src/main/java/com/wkrent/web/interceptor/LoginInterceptor.java
@@ -1,17 +1,25 @@
 package com.wkrent.web.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.wkrent.common.entity.base.BaseAjaxVO;
+import com.wkrent.common.entity.base.Constants;
 import com.wkrent.common.util.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * 登录拦截
  * @author chengan
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
     /**
      * 在业务处理器处理请求之前被调用
@@ -32,15 +40,29 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
         }
         response.setCharacterEncoding("utf-8");
         String token = request.getHeader("wkToken");
+        log.info("获取当前请求token：", token);
         if(null != token){
             boolean result = JwtUtil.verify(token);
             if(result){
                 return true;
             }
         }
-        String contextPath = request.getContextPath();
-        response.sendRedirect(contextPath + "/system/authFailed");
-        return true;
+        log.warn("当前token解析失败：", token);
+        //重置response
+        response.reset();
+        //设置编码格式
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+
+        PrintWriter pw = response.getWriter();
+        BaseAjaxVO baseAjaxVO = new BaseAjaxVO();
+        baseAjaxVO.setCode(Constants.TOKEN_AUTH_FAILED);
+        baseAjaxVO.setText(Constants.TOKEN_AUTH_FAILED_TEXT);
+        pw.write(JSON.toJSONString(baseAjaxVO));
+
+        pw.flush();
+        pw.close();
+        return false;
     }
 
     /**
