5f8adc488c330e643c466f88c7b00206652f0cb2
chengan <597049629@qq.com>
2019-02-13 21:29:18 +0800

    2019-02-10 房源查询新增商家名称返回

--- wkrent-common/src/main/java/com/wkrent/common/util/UUIDUtil.java
+++ wkrent-common/src/main/java/com/wkrent/common/util/UUIDUtil.java
@@ -52,4 +52,11 @@ public class UUIDUtil {
         }
         return buffer.toString();
     }
+
+    public static  void main(String[] args){
+        String[] str = getUUID(30);
+        for(String s : str){
+            System.out.println(s);
+        }
+    }
 }
