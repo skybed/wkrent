b9189772e40c74fac0f5d34675d993cafdb2066f
chengan <597049629@qq.com>
2019-04-27 17:54:37 +0800

    2019-02-17 租房说明二维码上传修改，后台自动关联附件信息

--- wkrent-business/src/main/java/com/wkrent/business/bg/datadict/service/BgDataDictService.java
+++ wkrent-business/src/main/java/com/wkrent/business/bg/datadict/service/BgDataDictService.java
@@ -1,19 +1,9 @@
 package com.wkrent.business.bg.datadict.service;
 
-import com.wkrent.common.entity.base.BaseAjaxVO;
-import com.wkrent.common.entity.vo.BgDataDictVO;
-
 /**
  * 数据字典类型
  * @author Administrator
  */
 public interface BgDataDictService {
 
-    /**
-     * 新增枚举类型信息
-     * @param dataDictVO 数据字典类型
-     * @param loginAccount 当前登录账号
-     * @return 新增后数据信息
-     */
-    BaseAjaxVO insert(BgDataDictVO dataDictVO, String loginAccount);
 }
--- wkrent-business/src/main/java/com/wkrent/business/bg/datadict/service/impl/BgDataDictServiceImpl.java
+++ wkrent-business/src/main/java/com/wkrent/business/bg/datadict/service/impl/BgDataDictServiceImpl.java
@@ -1,22 +1,7 @@
 package com.wkrent.business.bg.datadict.service.impl;
 
-import com.wkrent.business.bg.datadict.dao.BgDataDictDao;
 import com.wkrent.business.bg.datadict.service.BgDataDictService;
-import com.wkrent.common.entity.base.BaseAjaxVO;
-import com.wkrent.common.entity.po.BgDataDict;
-import com.wkrent.common.entity.po.BgDataDictValue;
-import com.wkrent.common.entity.vo.BgDataDictVO;
-import com.wkrent.common.entity.vo.BgDataDictValueVO;
-import com.wkrent.common.exception.WkRentException;
-import com.wkrent.common.util.BeanUtil;
-import com.wkrent.common.util.OperatorUtil;
-import com.wkrent.common.util.UUIDUtil;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * 数据字典类型
@@ -25,45 +10,4 @@ import java.util.List;
 @Service
 public class BgDataDictServiceImpl implements BgDataDictService{
 
-    @Autowired
-    private BgDataDictDao bgDataDictDao;
-    /**
-     * 新增枚举类型信息
-     *
-     * @param dataDictVO   数据字典类型
-     * @param loginAccount 当前登录账号
-     * @return 新增后数据信息
-     */
-    @Override
-    public BaseAjaxVO insert(BgDataDictVO dataDictVO, String loginAccount) {
-
-        BaseAjaxVO ajaxVO = new BaseAjaxVO();
-        //校验用户信息
-        if(checkDictInfo(dataDictVO)){
-            //校验当前枚举是否存在
-            BgDataDict dataDict = bgDataDictDao.findByType(dataDictVO.getBgDataDictType());
-            if(dataDict != null){
-                throw new WkRentException("新增分类失败，分类名称已存在");
-            }
-            BgDataDict bgDataDict = new BgDataDict();
-            bgDataDict.setBgDataDictId(UUIDUtil.getUUID());
-            bgDataDict.setBgDataDictType(dataDictVO.getBgDataDictType());
-            bgDataDict.setDescription(dataDictVO.getDescription());
-            OperatorUtil.setOperatorInfo(OperatorUtil.OperationType.Add, bgDataDict, loginAccount);
-            int result = bgDataDictDao.insert(bgDataDict);
-            if(result != 1){
-                throw new WkRentException("新增数据字典值失败，服务器异常！");
-            }
-            dataDictVO.setBgDataDictId(bgDataDict.getBgDataDictId());
-            ajaxVO.setResult(dataDictVO);
-        }
-        return ajaxVO;
-    }
-
-    private Boolean checkDictInfo(BgDataDictVO dataDictVO){
-        if(StringUtils.isBlank(dataDictVO.getBgDataDictType())){
-            throw new WkRentException("保存数据字典信息失败，分类名称不能为空");
-        }
-        return true;
-    }
 }
